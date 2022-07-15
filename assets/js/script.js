c2f2daf439f01604d53661157eee35924f68893a
Stephany M <sflores926@yahoo.com>
2022-07-15 16:14:12 -0400

    added js and css

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+    var questions= [
+      {
+        title: 'Commonly used data types DO NOT include: ',
+        options: [ 'strings', 'booleans', 'alerts', 'numbers'],
+        correctAnswer: 2
+        
+      },
+
+      {
+          title: 'The condition in an if/else statement is enclosed within _______.',
+          options: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+          correctAnswer: 2
+      },
+
+      {
+          title: 'Arrays in JavaScript can be used to store ______.',
+          options: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+          correctAnswer: 3
+      }, 
+
+      {
+          title: 'String values must be enclosed within _______ when being assigned to variables.',
+          options: ['commas', 'curly brackets', 'quotas', 'parentheses'],
+          correctAnswer: 2
+      },
+
+      {
+          title: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+          options: ['JavaScript', 'terminal/bash', 'for loops', 'console.log'],
+          correctAnswer: 3
+      }
+     
+    ];
+
+    console.log(questions);
