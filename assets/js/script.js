f5260af043d6481e666dbed8ccd65976feeeb07a
Stephany M <sflores926@yahoo.com>
2022-07-19 18:36:55 -0400

    fixed times added functions to check answers and moved timer and highscores section inside body of html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,23 @@
-    //assigned variables
-    var quizContainer = document.getElementById('quiz');
-    var resultsContainer = document.getElementById('results');
-    var submitButton = document.getElementById('submit');
-    var startButton = document.getElementById('start-btn-btn');
-    var scoresButton = document.getElementById('scores');
-    var secondsLeft = 60;
-    var countDown = document.getElementById('time-remaining')
+//assigned variables
+var questionsCont = document.getElementById('quizContainer');
+var questionTitle = document.getElementById("question-title");
+var option1 = document.getElementById("opt0");
+var option2 = document.getElementById("opt1");
+var option3 = document.getElementById("opt2");
+var option4 = document.getElementById("opt3");
+var answer = document.getElementById("checkAnswer");
+    // var resultsContainer = document.getElementById('results');
+    // var submitButton = document.getElementById('submit');
+var startButton = document.getElementById('start-btn-btn');
+var startCont = document.getElementById('quiz-header')
+var scoresButton = document.getElementById('scores');
+var secondsLeft = 60;
+var timeLeft = document.getElementById('timeLeft');
+var timer = document.getElementById("timer");
+var timeleft = 0;
 
-    var questions= [
+
+var questions = [
       {
         question: 'Commonly used data types DO NOT include: ',
         options: [ 'strings', 'booleans', 'alerts', 'numbers'],
@@ -42,80 +52,102 @@
     ];
 
     // console.log(questions);
+var questionIndex = 0;
+var correctAns = 0;
+var questionNum = 0;
+var scoreResult;
 
 
-
-
-function checkAnswer(event){
-        console.log(event.target.innerText);
+//to check if answer is right or wrong
+function checkAnswer(correctAnswer){
+        // console.log(event.target.innerText);
+        if(questions[questionIndex].answer === questions[questionIndex].options[answer]) {
+            correctAns++;
+            answer.textContent = "Right";
+        } else {
+            secondsLeft -= 10;
+            timeLeft.textContent = secondsLeft;
+            answer.textContent = "Wrong";
+        }
+        questionIndex++
+        if(questionIndex < questions.length) {
+            nextQuestion();
+        } else {
+            timesUp();
+        }
     
     }
 
+function option1() { checkAnswer(0);}
+function option2() { checkAnswer(1);}
+function option3() { checkAnswer(2);}
+function option4() { checkAnswer(3);}
+
+
+//Timer for quiz
 function setTime() {
+
+    questionIndex = 0;
+    secondsLeft = 60;
+    timeLeft.textContent = secondsLeft;
+
     var interval = setInterval(function(){
-        --secondsLeft;
-       countDown.textContent= "Time remaining; " + secondsLeft + " seconds";
+        secondsLeft--;
+        timeLeft.textContent= secondsLeft
         if (secondsLeft === 0) {
-             clearInterval(timerInterval);
+             clearInterval(interval);
+             if(questionIndex < questions.length -1) {
+                 timesUp();
+             }
             }
           }, 1000);
-        }
-document.getElementById("start-btn-btn").addEventListener("click", setTime);
+          makeQuiz();
+        };
+//  document.getElementById("start-btn-btn").addEventListener("click", setTime);
 
-function generateQuiz(){
+function timesUp() {
+    finalScore.textContent = correctAns;
+}
+
 
 //function for questions
 function makeQuiz() {
-    var output = [];
-    var options = [];
+   nextQuestion();
+}   
 
-   
-
-            for(letter of questions[0].options) {
-                options.push(
-                    '<label>'
-                        + '<button onclick= "checkAnswer(event)" value = "'+letter+'">'
-                        + letter + '</button>'
-                    + '</label>'
-                );
-            }
-            output.push(
-                '<div class = "question">' + questions[0].question + '</div>'
-                + '<div class = "options">' + options.join('') + '</div>'
-            );
-        
-console.log(output);
-
-
-    quizContainer.innerHTML = output.join('');
+function nextQuestion() {
+            questionTitle.textContent = questions[questionIndex].question;
+            option1.textContent = questions[questionIndex].options[0];
+            option2.textContent = questions[questionIndex].options[1];
+            option3.textContent = questions[questionIndex].options[2];
+            option4.textContent = questions[questionIndex].options[3];
     }
-
-
+console.log(questions[questionIndex].question);
+console.log(questions[questionIndex].options);
+// console.log(nextQuestion);
 
 //show results
-function showResults() {
-    var answerContainer = quizContainer.querySelectorAll('.options');
+// function showResults() {
+//     var answerContainer = quizContainer.querySelectorAll('.options');
 
-    var userAnswer = '';
-    var numCorrect = 0;
+//     var userAnswer = '';
+//     var numCorrect = 0;
 
-    for(var i=0; i<questions.length; i++){
-        userAnswer = (answerContainer[i].querySelector('input[name = question '+i+']:checked')||{}).value;
-        if(userAnswer === questions[i].correctAnswer){
-            numCorrect++;
-            answerContainer[i].style.color = 'lightgreen';
-        }
-        else{
-            answerContainer[i].style.color = 'red';
-        }
-    }
-    resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+//     for(var i=0; i<questions.length; i++){
+//         userAnswer = (answerContainer[i].querySelector('input[name = question '+i+']:checked')||{}).value;
+//         if(userAnswer === questions[i].correctAnswer){
+//             numCorrect++;
+//             answerContainer[i].style.color = 'lightgreen';
+//         }
+//         else{
+//             answerContainer[i].style.color = 'red';
+//         }
+//     }
+//     resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
 
-}
+// }
 
 
-makeQuiz();
-// submitButton.addEventListener('click', showResults);
-}
 
-startButton.addEventListener('click', generateQuiz);
+
+startButton.addEventListener('click', setTime);
